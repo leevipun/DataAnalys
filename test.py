929714136e44e1a60750cd0f95e88cb29594185c
Leevi Ilmari Puntanen <leevi.puntanen2@gmail.com>
2024-05-22 20:51:36 +0300

    Final touches

--- test.py
+++ test.py
@@ -4,5 +4,34 @@ import numpy as np
 files = pd.read_csv('vsk23clmaa.csv')
 
 avg = files.mean()
-
+print(len(files.index))
 print(avg)
+course_list = {}
+def averageCalc(csv):
+    df = pd.read_csv(csv)
+    pdf = df.apply(pd.to_numeric, errors='coerce')
+    avg = pdf.mean()
+    colum_len = 0
+    for i in range(1, len(avg)):
+            colum_len += 1
+            if np.isnan(avg[i]):
+                colum_len -= 1
+    true_avg = 0
+    print(avg)
+    for i in range(1, len(avg)):
+        if np.isnan(avg[i]):
+            continue
+        true_avg += avg[i]
+    for i in range(1, len(pdf.columns)):
+        courseName = pdf.columns[i]
+        amountofPeople = 0
+        for j in range(1, len(pdf.index)):
+            if np.isnan(pdf[pdf.columns[i]][j]):
+                continue
+            amountofPeople += 1
+        course_list[courseName] = amountofPeople
+    print(colum_len, true_avg)
+    return true_avg/colum_len
+
+averageCalc('vsk23dlmaa.csv')
+print(course_list)
--- text.py
+++ text.py
@@ -2,15 +2,15 @@ import pandas as pd
 import numpy as np
 
 avg_list = {}
+course_list = {}
 
 def make_average_file(files):
     for file in files:
         avg_list[file] = averageCalc(file)
     
     pd.DataFrame(avg_list, index=[0]).to_excel('pandas_data.xlsx', index=False)
-
-
-        
+    pd.DataFrame(course_list, index=[0]).to_excel('kurssien_osallistujamäärät.xlsx', index=False)    
+    pd.DataFrame(course_list, index=[0]).to_csv('kurssien_osallistujamäärät.csv', index=False)
 
 def averageCalc(csv):
     df = pd.read_csv(csv)
@@ -26,6 +26,15 @@ def averageCalc(csv):
         if np.isnan(avg[i]):
             continue
         true_avg += avg[i]
+    for i in range(1, len(pdf.columns)):
+        courseName = pdf.columns[i] + csv
+        amountofPeople = 0
+        for j in range(1, len(pdf.index)):
+            print(pdf[pdf.columns[i]][j])
+            if np.isnan(pdf[pdf.columns[i]][j]):
+                continue
+            amountofPeople += 1
+        course_list[courseName] = amountofPeople
     return true_avg/colum_len
 
     
@@ -122,3 +131,110 @@ avg_lista = ['vsk21aFysiikka.csv',
 'vsk23vLmab.csv',]
 make_average_file(avg_lista)
 print(avg_list)
+
+course_amount = {}
+
+def tt_course():
+     abitt = pd.read_csv('vsk21c.csv')
+     kakctt = pd.read_csv('vsk22c.csv')
+     kakdtt = pd.read_csv('vsk22d.csv')
+     koktt = pd.read_csv('vsk23d.csv')
+
+     kursYht = 0
+
+     for i in range(0, len(abitt)):
+        kursYht += abitt['Arvosanojen lukumäärä'][i]
+     for i in range(0, len(kakctt)):
+        kursYht += kakctt['Arvosanojen lukumäärä'][i]
+    
+     for i in range(0, len(kakdtt)):
+        kursYht += kakdtt['Arvosanojen lukumäärä'][i]
+     for i in range(0, len(koktt)):
+        kursYht += koktt['Arvosanojen lukumäärä'][i]
+
+     print(kursYht)
+     course_amount['Tiede- ja teknologialinja'] = kursYht
+
+
+def yl_course():
+    abiayl = pd.read_csv('vsk21a.csv')
+    kakayl = pd.read_csv('vsk22a.csv')
+    kokayl = pd.read_csv('vsk23a.csv')
+    kokbyl = pd.read_csv('vsk23b.csv')
+
+    kursYht = 0
+
+    for i in range(0, len(abiayl)):
+        kursYht += abiayl['Arvosanojen lukumäärä'][i]
+
+    for i in range(0, len(kakayl)):
+        kursYht += kakayl['Arvosanojen lukumäärä'][i]
+
+    for i in range(0, len(kokayl)):
+        kursYht += kokayl['Arvosanojen lukumäärä'][i]
+    
+    for i in range(0, len(kokbyl)):
+        kursYht += kokbyl['Arvosanojen lukumäärä'][i]
+
+    print(kursYht)
+    course_amount['Yleislinja'] = kursYht
+
+def yt_course():
+    abiyt = pd.read_csv('vsk21b.csv')
+    kakytt = pd.read_csv('vsk22b.csv')
+    kokytt = pd.read_csv('vsk23c.csv')
+
+    kursYht = 0
+
+    for i in range(0, len(abiyt)):
+        kursYht += abiyt['Arvosanojen lukumäärä'][i]
+    
+    for i in range(0, len(kakytt)):
+        kursYht += kakytt['Arvosanojen lukumäärä'][i]
+    
+    for i in range(0, len(kokytt)):
+        kursYht += kokytt['Arvosanojen lukumäärä'][i]
+    
+    print(kursYht)
+    course_amount['Yhteiskunta- ja talouslinja'] = kursYht
+
+def en_course():
+    abiden = pd.read_csv('vsk21d.csv')
+    abien = pd.read_csv('vsk21e.csv')
+    kaken = pd.read_csv('vsk22e.csv')
+    kakfen = pd.read_csv('vsk22f.csv')
+    koken = pd.read_csv('vsk23e.csv')
+    kokfen = pd.read_csv('vsk23f.csv')
+
+    kursYht = 0
+
+    for i in range(0, len(abiden)):
+        kursYht += abiden['Arvosanojen lukumäärä'][i]
+
+    for i in range(0, len(abien)):
+        kursYht += abien['Arvosanojen lukumäärä'][i]
+    
+    for i in range(0, len(kaken)):
+        kursYht += kaken['Arvosanojen lukumäärä'][i]
+
+    for i in range(0, len(kakfen)):
+        kursYht += kakfen['Arvosanojen lukumäärä'][i]
+
+    for i in range(0, len(koken)):
+        kursYht += koken['Arvosanojen lukumäärä'][i]
+
+    for i in range(0, len(kokfen)):
+        kursYht += kokfen['Arvosanojen lukumäärä'][i]
+    
+    print(kursYht)
+    course_amount['Englanninkielinen linja'] = kursYht
+
+def makeCourse_file():
+    pd.DataFrame(course_amount, index=[0]).to_excel('course_data.xlsx', index=False)
+    pd.DataFrame(course_amount, index=[0]).to_csv('course_data.csv', index=False)
+
+#tt_course()
+#yl_course()
+#yt_course()
+#en_course()
+#makeCourse_file()
